f69ab633954b2c334d9c4bbf29f1f4a346e99251
Brian Y Lin <me@brianl.in>
2018-08-04 11:55:35 -0700

    Add taxids and total count

--- count-freqs.py
+++ count-freqs.py
@@ -46,35 +46,27 @@ def main():
     'tpcloop': int,
     'varm': int
   })
-  message('done\n')
-
   trnas = trnas[trnas.primary].fillna('')
+  message('done\n')
 
   message('Parsing clades and taxonomic ranks...')
-  ranks = OrderedDict() # Dictionary that returns taxonomic rank given a clade
-  for rank in ['genus', 'family', 'order', 'subclass', 'class', 'subphylum', 'phylum', 'subkingdom', 'kingdom', 'domain']:
-    for clade in trnas[rank].unique():
-      if clade == '': continue
-      ranks[clade] = rank
+  taxonomy = pd.read_csv(taxonomy_file, sep = '\t', dtype = {'name': str, 'rank': str, 'taxid': str})
   message('done\n')
 
-
-  message('Counting freqs for {} clades...\n'.format(len(ranks)))
+  message('Counting freqs for {} clades...\n'.format(taxonomy.shape[0]))
   freqs = pd.DataFrame()
-  for clade in ranks.keys():
-    message('\tCounting base frequencies for {} {}...'.format(ranks[clade], clade))
-    current_rank = ranks[clade]
-    current_clade_trnas = trnas[trnas[current_rank] == clade]
+  for name, rank, taxid in taxonomy.itertuples(index = False):
+    message('\tCounting base frequencies for {} {}...'.format(rank, name))
+    current_clade_trnas = trnas[trnas[rank] == name]
     current_clade_freqs = count_freqs_isotypes(current_clade_trnas)
-    current_clade_freqs['clade'] = clade
-    current_clade_freqs['rank'] = ranks[clade]
+    current_clade_freqs['taxid'] = taxid
     freqs = freqs.append(current_clade_freqs, sort = True)
     message('done\n')
   message('Done\n')
   
   message('Exporting results to {}...'.format(output_file))
-  colorder = ['isotype', 'clade', 'rank', 'position'] + features
-  freqs = freqs.reindex(columns = colorder + [col for col in freqs.columns if col not in colorder])
+  colorder = ['taxid', 'isotype', 'position', 'total'] + features
+  freqs = freqs.reindex(columns = colorder)
   freqs.to_csv(path_or_buf = output_file, sep = '\t', index = False)
   message('done\n')
 
@@ -93,14 +85,17 @@ def count_freqs(trnas):
   '''Count freqas across positions given a set of tRNAs'''
   freqs = [] # to be converted into a pandas dataframe later
   for position in positions.values():
-    current_position = trnas.loc[:, position].value_counts()
+    current_position = trnas.loc[trnas[position].isin(features), position].value_counts()
+    current_position['total'] = sum(current_position)
     current_position['position'] = position
     freqs.append(current_position)
-  return pd.DataFrame(freqs, columns = features).fillna(0).astype(int).reset_index().rename(columns = {'index': 'position'})
+
+  return pd.DataFrame(freqs, columns = features + ['total']).fillna(0).astype(int).reset_index().rename(columns = {'index': 'position'})
 
 def parse_args():
   parser = argparse.ArgumentParser(description = "Generate table of tRNA features using tRNAscan-SE output")
   parser.add_argument('-t', '--input_tRNAs', default = 'tRNAs.tsv', help = '')
+  parser.add_argument('-c', '--taxonomy', default = 'taxonomy.tsv', help = '')
   parser.add_argument('-o', '--output_file', default = 'freqs-{}.tsv'.format(timestamp), help = '')
   return parser.parse_args()
 
@@ -110,6 +105,7 @@ if __name__ == '__main__':
   args = parse_args()
   input_tRNAs = args.input_tRNAs
   output_file = args.output_file
+  taxonomy_file = args.taxonomy
 
   isotypes = ['Ala', 'Arg', 'Asn', 'Asp', 'Cys', 'Gln', 'Glu', 'Gly', 'His', 'Ile', 'iMet', 'Leu', 'Lys', 'Met', 'Phe', 'Pro', 'Ser', 'Thr', 'Trp', 'Tyr', 'Val']
   positions = [('p1_72', '1:72'), ('p1', '1'), ('p2_71', '2:71'), ('p2', '2'), ('p3_70', '3:70'), ('p3', '3'), ('p4_69', '4:69'), ('p4', '4'), ('p5_68', '5:68'), ('p5', '5'), ('p6_67', '6:67'), ('p6', '6'), ('p7_66', '7:66'), ('p7', '7'), ('p8', '8'), ('p8_14', '8:14'), ('p9', '9'), ('p9_23', '9:23'), ('p10_25', '10:25'), ('p10', '10'), ('p10_45', '10:45'), ('p11_24', '11:24'), ('p11', '11'), ('p12_23', '12:23'), ('p12', '12'), ('p13_22', '13:22'), ('p13', '13'), ('p14', '14'), ('p15', '15'), ('p15_48', '15:48'), ('p16', '16'), ('p17', '17'), ('p17a', '17a'), ('p18', '18'), ('p18_55', '18:55'), ('p19', '19'), ('p19_56', '19:56'), ('p20', '20'), ('p20a', '20a'), ('p20b', '20b'), ('p21', '21'), ('p22', '22'), ('p22_46', '22:46'), ('p23', '23'), ('p24', '24'), ('p25', '25'), ('p26', '26'), ('p26_44', '26:44'), ('p27_43', '27:43'), ('p27', '27'), ('p28_42', '28:42'), ('p28', '28'), ('p29_41', '29:41'), ('p29', '29'), ('p30_40', '30:40'), ('p30', '30'), ('p31_39', '31:39'), ('p31', '31'), ('p32', '32'), ('p33', '33'), ('p34', '34'), ('p35', '35'), ('p36', '36'), ('p37', '37'), ('p38', '38'), ('p39', '39'), ('p40', '40'), ('p41', '41'), ('p42', '42'), ('p43', '43'), ('p44', '44'), ('p45', '45'), ('pV11_V21', 'V11:V21'), ('pV12_V22', 'V12:V22'), ('pV13_V23', 'V13:V23'), ('pV14_V24', 'V14:V24'), ('pV15_V25', 'V15:V25'), ('pV16_V26', 'V16:V26'), ('pV17_V27', 'V17:V27'), ('pV1', 'V1'), ('pV2', 'V2'), ('pV3', 'V3'), ('pV4', 'V4'), ('pV5', 'V5'), ('pV11', 'V11'), ('pV12', 'V12'), ('pV13', 'V13'), ('pV14', 'V14'), ('pV15', 'V15'), ('pV16', 'V16'), ('pV17', 'V17'), ('pV21', 'V21'), ('pV22', 'V22'), ('pV23', 'V23'), ('pV24', 'V24'), ('pV25', 'V25'), ('pV26', 'V26'), ('pV27', 'V27'), ('p46', '46'), ('p47', '47'), ('p48', '48'), ('p49_65', '49:65'), ('p49', '49'), ('p50_64', '50:64'), ('p50', '50'), ('p51_63', '51:63'), ('p51', '51'), ('p52_62', '52:62'), ('p52', '52'), ('p53_61', '53:61'), ('p53', '53'), ('p54', '54'), ('p54_58', '54:58'), ('p55', '55'), ('p56', '56'), ('p57', '57'), ('p58', '58'), ('p59', '59'), ('p60', '60'), ('p61', '61'), ('p62', '62'), ('p63', '63'), ('p64', '64'), ('p65', '65'), ('p66', '66'), ('p67', '67'), ('p68', '68'), ('p69', '69'), ('p70', '70'), ('p71', '71'), ('p72', '72'), ('p73', '73')]
--- resolve-consensus.py
+++ resolve-consensus.py
@@ -46,36 +46,26 @@ def main():
     'tpcloop': int,
     'varm': int
   })
-  message('done\n')
-
   trnas = trnas[trnas.primary]
-
+  message('done\n')
 
   message('Parsing clades and taxonomic ranks...')
-  ranks = OrderedDict() # Dictionary that returns taxonomic rank given a clade
-  for rank in ['genus', 'family', 'order', 'subclass', 'class', 'subphylum', 'phylum', 'subkingdom', 'kingdom', 'domain']:
-    for clade in trnas[rank].unique():
-      if pd.isnull(clade):
-        continue
-      ranks[clade] = rank
+  taxonomy = pd.read_csv(taxonomy_file, sep = '\t', dtype = {'name': str, 'rank': str, 'taxid': str})
   message('done\n')
 
-
-  message('Calculating consensus features for {} clades...\n'.format(len(ranks)))
+  message('Calculating consensus features for {} clades...\n'.format(taxonomy.shape[0]))
   consensus = pd.DataFrame()
-  for clade in ranks.keys():
-    message('\tResolving consensus for {} {}...'.format(ranks[clade], clade))
-    current_rank = ranks[clade]
-    current_clade_trnas = trnas[trnas[current_rank] == clade]
+  for name, rank, taxid in taxonomy.itertuples(index = False):
+    message('\tResolving consensus for {} ({})...'.format(name, rank))
+    current_clade_trnas = trnas[trnas[rank] == name]
     current_clade_consensus = resolve_consensus_isotypes(current_clade_trnas)
-    current_clade_consensus['clade'] = clade
-    current_clade_consensus['rank'] = ranks[clade]
+    current_clade_consensus['taxid'] = taxid
     consensus = consensus.append(current_clade_consensus, sort = True)
     message('done\n')
   message('Done\n')
   
   message('Exporting results to {}...'.format(output_file))
-  consensus = consensus.set_index(['isotype', 'clade', 'rank', 'position']).unstack('position')
+  consensus = consensus.set_index(['isotype', 'taxid', 'position']).unstack('position')
   consensus.columns = consensus.columns.get_level_values(1)
   consensus.to_csv(path_or_buf = output_file, sep = '\t')
   message('done\n')
@@ -129,6 +119,7 @@ def get_candidate_features(features, combos):
 def parse_args():
   parser = argparse.ArgumentParser(description = "Generate table of tRNA features using tRNAscan-SE output")
   parser.add_argument('-t', '--input_tRNAs', default = 'tRNAs.tsv', help = '')
+  parser.add_argument('-c', '--taxonomy', default = 'taxonomy.tsv', help = '')
   parser.add_argument('-o', '--output_file', default = 'consensus-{}.tsv'.format(timestamp), help = '')
   return parser.parse_args()
 
@@ -138,6 +129,7 @@ if __name__ == '__main__':
   args = parse_args()
   input_tRNAs = args.input_tRNAs
   output_file = args.output_file
+  taxonomy_file = args.taxonomy
 
   isotypes = ['Ala', 'Arg', 'Asn', 'Asp', 'Cys', 'Gln', 'Glu', 'Gly', 'His', 'Ile', 'iMet', 'Leu', 'Lys', 'Met', 'Phe', 'Pro', 'Ser', 'Thr', 'Trp', 'Tyr', 'Val']
   positions = [('p1_72', '1:72'), ('p1', '1'), ('p2_71', '2:71'), ('p2', '2'), ('p3_70', '3:70'), ('p3', '3'), ('p4_69', '4:69'), ('p4', '4'), ('p5_68', '5:68'), ('p5', '5'), ('p6_67', '6:67'), ('p6', '6'), ('p7_66', '7:66'), ('p7', '7'), ('p8', '8'), ('p8_14', '8:14'), ('p9', '9'), ('p9_23', '9:23'), ('p10_25', '10:25'), ('p10', '10'), ('p10_45', '10:45'), ('p11_24', '11:24'), ('p11', '11'), ('p12_23', '12:23'), ('p12', '12'), ('p13_22', '13:22'), ('p13', '13'), ('p14', '14'), ('p15', '15'), ('p15_48', '15:48'), ('p16', '16'), ('p17', '17'), ('p17a', '17a'), ('p18', '18'), ('p18_55', '18:55'), ('p19', '19'), ('p19_56', '19:56'), ('p20', '20'), ('p20a', '20a'), ('p20b', '20b'), ('p21', '21'), ('p22', '22'), ('p22_46', '22:46'), ('p23', '23'), ('p24', '24'), ('p25', '25'), ('p26', '26'), ('p26_44', '26:44'), ('p27_43', '27:43'), ('p27', '27'), ('p28_42', '28:42'), ('p28', '28'), ('p29_41', '29:41'), ('p29', '29'), ('p30_40', '30:40'), ('p30', '30'), ('p31_39', '31:39'), ('p31', '31'), ('p32', '32'), ('p33', '33'), ('p34', '34'), ('p35', '35'), ('p36', '36'), ('p37', '37'), ('p38', '38'), ('p39', '39'), ('p40', '40'), ('p41', '41'), ('p42', '42'), ('p43', '43'), ('p44', '44'), ('p45', '45'), ('pV11_V21', 'V11:V21'), ('pV12_V22', 'V12:V22'), ('pV13_V23', 'V13:V23'), ('pV14_V24', 'V14:V24'), ('pV15_V25', 'V15:V25'), ('pV16_V26', 'V16:V26'), ('pV17_V27', 'V17:V27'), ('pV1', 'V1'), ('pV2', 'V2'), ('pV3', 'V3'), ('pV4', 'V4'), ('pV5', 'V5'), ('pV11', 'V11'), ('pV12', 'V12'), ('pV13', 'V13'), ('pV14', 'V14'), ('pV15', 'V15'), ('pV16', 'V16'), ('pV17', 'V17'), ('pV21', 'V21'), ('pV22', 'V22'), ('pV23', 'V23'), ('pV24', 'V24'), ('pV25', 'V25'), ('pV26', 'V26'), ('pV27', 'V27'), ('p46', '46'), ('p47', '47'), ('p48', '48'), ('p49_65', '49:65'), ('p49', '49'), ('p50_64', '50:64'), ('p50', '50'), ('p51_63', '51:63'), ('p51', '51'), ('p52_62', '52:62'), ('p52', '52'), ('p53_61', '53:61'), ('p53', '53'), ('p54', '54'), ('p54_58', '54:58'), ('p55', '55'), ('p56', '56'), ('p57', '57'), ('p58', '58'), ('p59', '59'), ('p60', '60'), ('p61', '61'), ('p62', '62'), ('p63', '63'), ('p64', '64'), ('p65', '65'), ('p66', '66'), ('p67', '67'), ('p68', '68'), ('p69', '69'), ('p70', '70'), ('p71', '71'), ('p72', '72'), ('p73', '73')]
