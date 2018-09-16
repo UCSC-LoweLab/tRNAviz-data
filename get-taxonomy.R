397c54f4feec820a8d5e55c36a3b457433993e86
Brian Y Lin <me@brianl.in>
2018-09-15 22:05:09 -0700

    Standardize clades and generalize file checking

--- get-taxonomy.R
+++ get-taxonomy.R
@@ -1,4 +1,4 @@
-#!/usr/bin/env Rscript
+# This script is best run in a REPL using appropriate steps 
 library(taxize)
 library(stringr)
 library(plyr)
@@ -11,9 +11,23 @@ taxids = genome_table$taxid %>% unlist %>% unname %>% unique
 
 # grab classifications for each genome assembly
 classifs = classification(taxids, db = "ncbi")
+save.image('taxonomy.RData')
 
 # parse classifications to usable format
-taxonomy = ldply(1:length(classifs), function(i) {
+taxonomy = data.frame('superkingdom' = character(0), 
+                      'kingdom' = character(0), 
+                      'subkingdom' = character(0), 
+                      'phylum' = character(0), 
+                      'subphylum' = character(0), 
+                      'class' = character(0), 
+                      'subclass' = character(0), 
+                      'order' = character(0), 
+                      'family' = character(0), 
+                      'genus' = character(0), 
+                      'species' = character(0), 
+                      'varietas' = character(0),
+                      stringsAsFactors = FALSE) %>% 
+  rbind.fill(ldply(1:length(classifs), function(i) {
   taxid = names(classifs)[i]
   classif = classifs[[i]]
   if (taxid %in% classif$id) assembly = classif[classif$id == taxid, ]$name
@@ -24,21 +38,21 @@ taxonomy = ldply(1:length(classifs), function(i) {
     rbind(c(name = taxid, rank = 'taxid')) %>%
     rbind(c(name = assembly, rank = 'assembly')) %>%
     spread(rank, name, fill = '')
-})
+}))
 
 taxonomy = taxonomy %>% rename(domain = superkingdom)
+taxonomy[is.na(taxonomy)] = ''
 
-if (any(is.na(taxonomy))) taxonomy = taxonomy %>% replace_na(list(domain = '', phylum = '', subphylum = '', class = '', subclass = '', order = '', family = '', genus = '', species = '', assembly = ''))
 # add classifications back to original genome table
 genome_table = genome_table %>% left_join(taxonomy, by = 'taxid') %>%
-  select(dbname, dirname, taxid, assembly, species, genus, family, order, subclass, class, subphylum, phylum, domain)
+  select(dbname, dirname, taxid, assembly, varietas, species, genus, family, order, subclass, class, subphylum, phylum, subkingdom, kingdom, domain)
 
 # filter out entries for which files are missing
+# add modifier to filename for higher eukaryotes (some include "-detailed" for out files)
 genome_table = genome_table %>%
-  filter(file.exists(paste0('/projects/lowelab/users/blin/identity/bact/data/ss/', dbname, '-tRNAs.ss')),
-    file.exists(paste0('/projects/lowelab/users/blin/identity/bact/data/out/', dbname, '-tRNAs.out')),
-    file.exists(paste0('/projects/lowelab/users/blin/identity/bact/data/tRNAs/', dbname, '-tRNAs.fa')),
-    file.exists(paste0('/projects/lowelab/users/blin/identity/bact/data/iso/', dbname, '-tRNAs.iso')))
+  filter(file.exists(paste0('data/ss/', dbname, '-tRNAs.ss')),
+    file.exists(paste0('data/out/', dbname, '-tRNAs.out')) | file.exists(paste0('data/out/', dbname, '-tRNAs-detailed.out')),
+    file.exists(paste0('data/iso/', dbname, '-tRNAs.iso')))
 
 write.table(genome_table, file = 'genomes.tsv', quote = FALSE, sep = '\t', row.names = FALSE)
 
