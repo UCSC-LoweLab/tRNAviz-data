c44239aacf897fef3ccab2c8c9f4485d82caa6f0
Brian Y Lin <me@brianl.in>
2018-10-17 16:33:08 -0700

    Expand number of genomes included, add domain column

--- get-taxonomy.R
+++ get-taxonomy.R
@@ -37,18 +37,17 @@ taxonomy = ldply(1:length(classifs), function(i) {
   })
 })
 
-# remove duplicates and order columns
-taxonomy = taxonomy %>% 
-  replace(., is.na(.), '') %>%
-  unique %>%
-  select(name, rank, taxid, kingdom, subkingdom, phylum, subphylum, class, subclass, order, family, genus, species, assembly)
+# remove duplicates, order / enforce columns
+df = data.frame(name = character(), rank = character(), taxid = character(), domain = character(), kingdom = character(), subkingdom = character(), phylum = character(), subphylum = character(), class = character(), subclass = character(), order = character(), family = character(), genus = character(), species = character(), assembly = character(), stringsAsFactors = FALSE)
+df = bind_rows(df, taxonomy %>% replace(., is.na(.), '') %>% unique)
+taxonomy = df %>% replace(., is.na(.), '')
 
 write.table(taxonomy, file = 'taxonomy.tsv', quote = FALSE, sep = '\t', row.names = FALSE)
 
 
 # add classifications back to original genome table
 genome_table = genome_table %>% left_join(taxonomy %>% filter(assembly != ''), by = 'taxid') %>%
-  select(dbname, taxid, name, kingdom, subkingdom, phylum, subphylum, class, subclass, order, family, genus, species, assembly)
+  select(dbname, taxid, name, domain, kingdom, subkingdom, phylum, subphylum, class, subclass, order, family, genus, species, assembly)
 
 # filter out entries for which files are missing
 # add modifier to filename for higher eukaryotes (some include "-detailed" for out files)
@@ -57,7 +56,7 @@ genome_table = genome_table %>%
     file.exists(paste0('data/out/', dbname, '-tRNAs.out')) | file.exists(paste0('data/out/', dbname, '-tRNAs-detailed.out')),
     file.exists(paste0('data/iso/', dbname, '-tRNAs.iso')))
 
-# then filter out anything that isn't "representative" or "reference"
+# Filter out anything that isn't "representative" or "reference"
 # stats:
 # 3344 assemblies are represented by 1481 representative species. 
 # 890 assemblies belong to 687 unrepresented species distributed as such, where "single" means these species have only 1 corresponding assembly.
@@ -78,6 +77,6 @@ approved_taxids = c('559292')
 
 ncbi_refs = read.delim('/projects/lowelab/db/ncbi/genomes/ASSEMBLY_REPORTS/assembly_summary_genbank.txt', stringsAsFactors = FALSE, sep = '\t', comment.char = '#', header = FALSE) %>% 
   filter(V5 %in% c('representative genome', 'reference genome'))
-genome_table = genome_table %>% filter(taxid %in% ncbi_refs$V7 | taxid %in% approved_taxids)
+genome_table = genome_table %>% filter(taxid %in% ncbi_refs$V6 | taxid %in% ncbi_refs$V7 | taxid %in% approved_taxids)
 
 write.table(genome_table, file = 'genomes.tsv', quote = FALSE, sep = '\t', row.names = FALSE)
